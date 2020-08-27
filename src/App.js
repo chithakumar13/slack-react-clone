632d80db5f4e0b620151235bebb05b4035f78178
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-08-27 22:31:24 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { SideBar } from "./components/SideBar/SideBar.component";
+
 import './App.css';
 
 function App() {
   return (
     <div >
-      App Component.
+      <SideBar />
     </div>
   );
 }

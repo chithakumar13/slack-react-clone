5e2292bbc3420ce029bc7c5c2a01592200801b99
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-08-28 01:14:33 +0530

    create channel

---
+++ src/components/SideBar/Channels/Channels.component.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import firebase from "../../../server/firebase";
+
+import './Channels.css';
+import { Menu, Icon, Modal, Button, Form, Segment } from 'semantic-ui-react';
+
+const Channels = (props) => {
+    const [modalOpenState, setModalOpenState] = useState(false);
+    const [channelAddState, setChannelAddState] = useState({ name: '', description: '' });
+    const [isLoadingState, setLoadingState] = useState(false);
+
+    const channelsRef = firebase.database().ref("channels");
+
+    const openModal = () => {
+        setModalOpenState(true);
+    }
+
+    const closeModal = () => {
+        setModalOpenState(false);
+    }
+
+    const checkIfFormValid = () => {
+        return channelAddState && channelAddState.name && channelAddState.description;
+    }
+
+    const onSubmit = () => {
+
+        if (!checkIfFormValid()) {
+            return;
+        }
+
+        const key = channelsRef.push().key;
+
+        const channel = {
+            id: key,
+            name: channelAddState.name,
+            description: channelAddState.description,
+            created_by: {
+                name: props.user.displayName,
+                avatar: props.user.photoURL
+            }
+        }
+        setLoadingState(true);
+        channelsRef.child(key)
+            .update(channel)
+            .then(() => {
+                setChannelAddState({ name: '', description: '' });
+                setLoadingState(false);
+                closeModal();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    const handleInput = (e) => {
+
+        let target = e.target;
+        setChannelAddState((currentState) => {
+            let updatedState = { ...currentState };
+            updatedState[target.name] = target.value;
+            return updatedState;
+        })
+    }
+
+    return <> <Menu.Menu>
+        <Menu.Item>
+            <span>
+                <Icon name="exchange" /> Channels
+            </span>
+            (0)
+        </Menu.Item>
+        <Menu.Item>
+            <span className="clickable" onClick={openModal} >
+                <Icon name="add" /> ADD
+            </span>
+        </Menu.Item>
+    </Menu.Menu>
+        <Modal open={modalOpenState} onClose={closeModal}>
+            <Modal.Header>
+                Create Channel
+            </Modal.Header>
+            <Modal.Content>
+                <Form onSubmit={onSubmit}>
+                    <Segment stacked>
+                        <Form.Input
+                            name="name"
+                            value={channelAddState.name}
+                            onChange={handleInput}
+                            type="text"
+                            placeholder="Enter Channel Name"
+                        />
+                        <Form.Input
+                            name="description"
+                            value={channelAddState.description}
+                            onChange={handleInput}
+                            type="text"
+                            placeholder="Enter Channel Description"
+                        />
+                    </Segment>
+                </Form>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button loading={isLoadingState} onClick={onSubmit}>
+                    <Icon name="checkmark" /> Save
+                </Button>
+                <Button onClick={closeModal}>
+                    <Icon name="remove" /> Cancel
+                </Button>
+            </Modal.Actions>
+        </Modal>
+    </>
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Channels);

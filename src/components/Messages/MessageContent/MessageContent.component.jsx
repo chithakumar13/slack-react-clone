d848270b67c4236a30f1d7fe3d152fc1991a7e81
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-09-04 22:14:24 +0530

    Image Upload

--- src/components/Messages/MessageContent/MessageContent.component.jsx
+++ src/components/Messages/MessageContent/MessageContent.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Comment } from "semantic-ui-react";
+import { Comment,Image } from "semantic-ui-react";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 
@@ -13,9 +13,11 @@ const MessageContent = (props) => {
     return <Comment>
         <Comment.Avatar src={props.message.user.avatar} />
         <Comment.Content className={props.ownMessage ? "ownMessage" : null}>
-            <Comment.Author as ="a">{props.message.user.name}</Comment.Author>
+            <Comment.Author as="a">{props.message.user.name}</Comment.Author>
             <Comment.Metadata>{timeAgo.format(props.message.timestamp)}</Comment.Metadata>
-            <Comment.Text>{props.message.content}</Comment.Text>
+            {props.message.image ? <Image src={props.message.image} /> :
+                <Comment.Text>{props.message.content}</Comment.Text>
+            }
         </Comment.Content>
     </Comment>
 }
--- src/components/Messages/MessageInput/MessageInput.component.jsx
+++ src/components/Messages/MessageInput/MessageInput.component.jsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 import { Segment, Input, Button } from "semantic-ui-react";
 import firebase from "../../../server/firebase";
 import { connect } from "react-redux";
+import { ImageUpload } from "../ImageUpload/ImageUpload.component"
+import uuidv4 from "uuid/v4";
 
 const MessageInput = (props) => {
 
     const messageRef = firebase.database().ref('messages');
 
+    const storageRef = firebase.storage().ref();
+
     const [messageState, setMessageState] = useState("");
 
-    const createMessageInfo = () => {
+    const [fileDialogState, setFileDialog] = useState(false);
+
+    const createMessageInfo = (downloadUrl) => {
         return {
             user: {
                 avatar: props.user.photoURL,
@@ -17,15 +23,16 @@ const MessageInput = (props) => {
                 id: props.user.uid
             },
             content: messageState,
+            image : downloadUrl || "",
             timestamp: firebase.database.ServerValue.TIMESTAMP
         }
     }
 
-    const onSubmit = () => {
-        if (messageState) {
+    const sendMessage = (downloadUrl) => {
+        if (messageState || downloadUrl) {
             messageRef.child(props.channel.id)
                 .push()
-                .set(createMessageInfo())
+                .set(createMessageInfo(downloadUrl))
                 .then(() => setMessageState(""))
                 .catch((err) => console.log(err))
         }
@@ -38,9 +45,24 @@ const MessageInput = (props) => {
 
     const createActionButtons = () => {
         return <>
-            <Button icon="send" onClick={onSubmit} />
-            <Button icon="upload" />
+            <Button icon="send" onClick={() => {sendMessage() }} />
+            <Button icon="upload" onClick={() => setFileDialog(true)} />
         </>
+    }
+
+    const uploadImage = (file, contentType) => {
+
+        const filePath = `chat/images/${uuidv4()}.jpg`;
+
+        storageRef.child(filePath).put(file, { contentType: contentType })
+            .then((data) => {
+                data.ref.getDownloadURL()
+                .then((url) => {
+                    sendMessage(url);
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
     }
 
     return <Segment>
@@ -52,6 +74,7 @@ const MessageInput = (props) => {
             label={createActionButtons()}
             labelPosition="right"
         />
+        <ImageUpload uploadImage={uploadImage} open={fileDialogState} onClose={() => setFileDialog(false)} />
     </Segment>
 }
 
